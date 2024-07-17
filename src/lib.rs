a281d7be5172c94ea88880b648d98efa2bbecc11
caelwarner <caelawarner@gmail.com>
2024-07-17 23:39:50 +0100

    Improved library
    - Node now handles the main loop of receiving messages and then handling them with a MessageHandler
    - Node::new initializes the node using the init and init_ok messages

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use color_eyre::Result;
 use std::fmt::Debug;
+use std::io::{BufRead, StdoutLock, Write};
 use std::marker::PhantomData;
 use serde::{Deserialize, Serialize};
+use serde::de::DeserializeOwned;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Message<P> {
@@ -63,9 +65,59 @@ pub struct InitOk {
     pub _marker: PhantomData<bool>,
 }
 
-#[derive(Debug, Default)]
-pub struct Node<S> {
+pub type MessageHandler<S, P> = fn(&mut Node<'_, S, P>, Message<P>) -> Result<()>;
+
+#[derive(Debug)]
+pub struct Node<'a, S, P> {
     pub node_id: String,
     pub node_ids: Vec<String>,
     pub state: S,
+    handler: MessageHandler<S, P>,
+    stdout: StdoutLock<'a>,
+}
+
+impl<S, P> Node<'_, S, P>
+    where P: Serialize + DeserializeOwned,
+{
+    pub fn new(state: S, handler: MessageHandler<S, P>) -> Result<Self>
+    {
+        let mut stdin = std::io::stdin().lock();
+        let mut stdout = std::io::stdout().lock();
+
+        let mut line = String::new();
+        stdin.read_line(&mut line)?;
+
+        let init_message = serde_json::from_str::<Message<Init>>(&line)?;
+        let (init_message, payload) = init_message.take_payload();
+
+        let init_ok_message = init_message.into_reply(InitOk::default());
+        serde_json::to_writer(&mut stdout, &init_ok_message)?;
+        stdout.write_all(b"\n")?;
+
+        Ok(Self {
+            node_id: payload.node_id,
+            node_ids: payload.node_ids,
+            state,
+            handler,
+            stdout,
+        })
+    }
+
+    pub fn run(mut self) -> Result<()> {
+        for line in std::io::stdin().lock().lines() {
+            let line = line?;
+
+            let message = serde_json::from_str::<Message<P>>(&line)?;
+            (self.handler)(&mut self, message)?;
+        }
+
+        Ok(())
+    }
+
+    pub fn send(&mut self, message: &Message<P>) -> Result<()> {
+        serde_json::to_writer(&mut self.stdout, &message)?;
+        self.stdout.write_all(b"\n")?;
+
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,23 @@
 use color_eyre::Result;
-use std::io::{BufRead, Write};
-use color_eyre::eyre::OptionExt;
 use serde::{Deserialize, Serialize};
-use gossip_glomers::{Init, InitOk, Message, Node};
+use gossip_glomers::{Message, Node};
 
 fn main() -> Result<()> {
-    let mut stdin = std::io::stdin().lock().lines();
-    let mut stdout = std::io::stdout().lock();
+    let node = Node::new((), message_handler)?;
 
-    let line = stdin.next().ok_or_eyre("Missing init message")??;
+    node.run()
+}
 
-    let init_message = serde_json::from_str::<Message<Init>>(&line)?;
-    let (init_message, payload) = init_message.take_payload();
-    let _node = Node {
-        node_id: payload.node_id,
-        node_ids: payload.node_ids,
-        state: (),
-    };
+fn message_handler(node: &mut Node<(), Payload>, message: Message<Payload>) -> Result<()> {
+    let (message, payload) = message.take_payload();
 
-
-    let init_ok_message = init_message.into_reply(InitOk::default());
-    serde_json::to_writer(&mut stdout, &init_ok_message)?;
-    stdout.write_all(b"\n")?;
-
-    for line in stdin {
-        let line = line?;
-
-        let message = serde_json::from_str::<Message<Payload>>(&line)?;
-        let (message, payload) = message.take_payload();
-        match payload {
-            Payload::Echo { echo } => {
-                serde_json::to_writer(&mut stdout, &message.into_reply(Payload::EchoOk { echo }))?;
-                stdout.write_all(b"\n")?;
-            },
-            Payload::EchoOk { .. } => {},
-        }
+    match payload {
+        Payload::Echo { echo } => {
+            node.send(&message.into_reply(Payload::EchoOk { echo }))?;
+        },
+        Payload::EchoOk { .. } => {
+            // Do nothing
+        },
     }
 
     Ok(())
